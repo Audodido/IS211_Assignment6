a1b399ee21902f8071b7c65b2a36bc50f37db221
connor <chanwick@gmail.com>
2021-10-06 15:35:06 -0400

    Through Part III done

--- conversions.py
+++ conversions.py
@@ -1,13 +1,45 @@
 
+# celsius
 def convertCelsiusToKelvin(celsius):
     """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins"""
-    kelvins = celsius + 273.15
+    kelvin = celsius + 273.15
     
-    return kelvins
+    return kelvin
 
 
 def convertCelsiusToFahrenheit(celsius):
     """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit"""
-    fahrenheit = 0
+    fahrenheit = celsius * 9 / 5 + 32
     
     return fahrenheit
+
+
+# farenheit
+def convertFarenheitToKelvin(farenheit):
+    """Takes in a float representing a Farenheit measurement, and returns that temperature converted into Kelvin"""
+    kelvin = (farenheit + 459.67) * 5/9
+
+    return kelvin
+
+
+def convertFarenheitToCelsius(farenheit):
+    """Takes in a float representing a Farenheit measurement, and returns that temperature converted into Celsius"""
+    celsius = (farenheit - 32) * 5/9
+
+    return celsius
+
+
+# kelvin
+def convertKelvinToFarenheit(kelvin):
+    """Takes in a float representing a Farenheit measurement, and returns that temperature converted into Kelvin"""
+    farenheit = kelvin * 9/5 - 459.67
+
+    return round(farenheit, 2)
+
+
+def convertKelvinToCelsius(kelvin):
+    """Takes in a float representing a Farenheit measurement, and returns that temperature converted into Celsius"""
+    celsius = kelvin - 273.15
+
+    return round(celsius, 2)
+
--- tests.py
+++ tests.py
@@ -3,16 +3,31 @@ import unittest
 
 
 
-# def convertCelsiusToKelvin(temp):
-#     return 0.0
-
 
 class TestConverters(unittest.TestCase):
 
-    def test_con_cel_kel(self):
-        self.assertEquals(conversions.convertCelsiusToKelvin(30), 303.15 )
+    # celcius
+    def test_convertCelsiusToKelvin(self):
+        self.assertEqual(conversions.convertCelsiusToKelvin(30), 303.15)
+
+    def test_convertCelsiusToFahrenheit(self):
+        self.assertEqual(conversions.convertCelsiusToFahrenheit(30), 86.0)
+
+    # farenheit
+    def test_convertFarenheitToKelvin(self):
+        self.assertEqual(conversions.convertFarenheitToKelvin(50), 283.15)
+
+    def test_convertFarenheitToCelsius(self):
+        self.assertEqual(conversions.convertFarenheitToCelsius(50), 10)
+
+    # kelvin
+    def test_convertKelvinToFarenheit(self):
+        self.assertEqual(conversions.convertKelvinToFarenheit(300), 80.33)
+    
+    def test_convertKelvinToCelsius(self):
+        self.assertEqual(conversions.convertKelvinToCelsius(300), 26.85)
+
 
 
 if __name__ == "__main__":
-
     unittest.main()
