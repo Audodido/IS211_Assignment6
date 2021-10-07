71237b131caa8bd03d4530035254e45327753e2e
connor <chanwick@gmail.com>
2021-10-07 11:59:40 -0400

    Test

--- conversions_refactored.py
+++ conversions_refactored.py
@@ -8,24 +8,35 @@ def convert(fromUnit, toUnit, value):
 
     fromUnit, toUnit = fromUnit.lower(), toUnit.lower() #make sure strings are lower case
 
-    # converting miles to meters/yards
-    if fromUnit == 'miles':
-        if toUnit == 'yards':
-            converted_val = round(value * 1760.0, 2)
-        elif toUnit == 'meters':
-            converted_val = round(value * 1609.344, 2)
-        # else:
-        #     raise TypeError(f"can't convert {fromUnit} to {toUnit}")
+    miles_yards = 1760.0
+
+    # # converting miles to meters/yards
+    # if fromUnit == 'miles':
+    #     if toUnit == 'yards':
+    #         converted_val = round(value * 1760.0, 2)
+    #     elif toUnit == 'meters':
+    #         converted_val = round(value * 1609.344, 2)
+    #     # else:
+    #     #     raise TypeError(f"can't convert {fromUnit} to {toUnit}")
 
 
-    # converting yards to miles/meters
-    if fromUnit == 'yards':
-        if toUnit == 'miles':
-            converted_val = round(value / 1760.0, 2)
-        elif toUnit == 'meters':
-            converted_val = round(value / 1.094, 2)
+    # # converting yards to miles/meters
+    # elif fromUnit == 'yards':
+    #     if toUnit == 'miles':
+    #         converted_val = round(value / 1760.0, 2)
+    #     elif toUnit == 'meters':
+    #         converted_val = round(value / 1.094, 2)
 
 
+    # # # converting meters to miles/yards
+    # elif fromUnit == 'meters':
+    #     if toUnit == 'miles':
+    #         converted_val = round(value / 1609.344, 2)
+    #     elif toUnit == 'yards':
+    #         converted_val = round(value * 1.094, 2)
+    
+    # else:
+    #     converted_val = 0 #change to error
 
     return float(converted_val)
 
--- test_conversions_refactored.py
+++ test_conversions_refactored.py
@@ -3,19 +3,9 @@ from conversions_refactored import convert
 import unittest
 
 
-
-
-
 class TestRefactoredMethod(unittest.TestCase):
 
-    def test_ConvertMilesToYards(self):
-        self.assertEqual(convert('miles', 'yards', 2.0), 3520.0)
 
-    def test_ConvertMilesToMeters(self):
-        self.assertEqual(convert('miles', 'meters', 2.0), 3218.69)
-
-    def test_ConvertYardsToMiles(self):
-        self.assertEqual(convert('yards', 'miles', 100.0), .06)
 
 
 
